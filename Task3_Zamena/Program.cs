ff7a290ff01a90bfbf2ec5b2f63a4ac958f73205
Inna <shvartz_i@mail.ru>
2022-01-11 17:17:13 +0300

    Task3 added

---
+++ Task3_Zamena/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите длину массива: ");
+int length = int.Parse(Console.ReadLine() ?? "0");
+int[]  array = new int [length];
+
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(0, 10);
+
+for (int i = 0; i < array.Length; i++)
+    Console.WriteLine(array[i]);
