85ada9ea3f9c62f5376c22f18ad64698c5556ae7
Inna <shvartz_i@mail.ru>
2022-01-11 18:01:48 +0300

    Task3_Zamena modifyed

--- Task3_Zamena/Program.cs
+++ Task3_Zamena/Program.cs
@@ -1,9 +1,21 @@
-﻿Console.Write("Введите длину массива: ");
+﻿// 34. Написать программу замену элементов массива на противоположные
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write($"{array[i],4}  ");
+}
+
+Console.Write("Введите длину массива: ");
 int length = int.Parse(Console.ReadLine() ?? "0");
-int[]  array = new int [length];
+int[] array = new int[length];
 
 for (int i = 0; i < array.Length; i++)
     array[i] = new Random().Next(0, 10);
 
+PrintArray(array);
+Console.WriteLine();
+
 for (int i = 0; i < array.Length; i++)
-    Console.WriteLine(array[i]);
+    array[i] = array[i] * -1;
+
+PrintArray(array);
