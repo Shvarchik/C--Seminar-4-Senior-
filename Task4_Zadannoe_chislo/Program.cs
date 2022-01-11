eb939330e2e3f8bb5cefde5e7b5c1005c40f7c3b
Inna <shvartz_i@mail.ru>
2022-01-11 20:34:20 +0300

    Task4 added

---
+++ Task4_Zadannoe_chislo/Program.cs
@@ -0,0 +1,36 @@
+﻿// 35. Определить, присутствует ли в заданном массиве, некоторое число
+int[] EnterArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"Введите {i + 1} элемент массива: ");
+        array[i] = int.Parse(Console.ReadLine() ?? "0");
+    }
+
+    return array;
+}
+
+bool ContainNumber(int[] array, int N)
+{
+    for (int i = 0; i < array.Length; i++)
+
+        if (array[i] == N)
+            return true;
+
+    return false;
+
+}
+
+Console.WriteLine("Введите размер массива:");
+int length = int.Parse(Console.ReadLine() ?? "0");
+int[] array = EnterArray(length);
+
+Console.Write("Введите искомое число: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+if (ContainNumber(array, N) == true)
+    Console.WriteLine($"Массив содержит {N}");
+else
+ Console.WriteLine($"Массив не содержит {N}");
+
+
