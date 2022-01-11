35ce8a06a9340f23c77d8a3b580ed057a0e4a3f0
Inna <shvartz_i@mail.ru>
2022-01-12 02:00:10 +0300

    Task7 added

---
+++ Task7_Summa_nechet_poz/Program.cs
@@ -0,0 +1,19 @@
+﻿// 38. Найти сумму чисел одномерного массива стоящих на нечетной позиции
+// C хитрым вводом сразу всего массива через пробел 
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine("Ваш массив:");
+    for (int i = 0; i < array.Length; i++)
+        Console.Write($"{array[i],4}");
+    Console.WriteLine();
+}
+Console.WriteLine("Введите массив через пробел:");
+int[] array = (Console.ReadLine() ?? "0").Split(' ').Select(int.Parse).ToArray(); // ЭТО ОН
+PrintArray (array);
+int sum = 0;
+for (int i = 0; i < array.Length; i = i + 2)
+    sum = sum + array[i];
+Console.WriteLine ($"Сумма чисел на нечетных позициях {sum}");
+
+
