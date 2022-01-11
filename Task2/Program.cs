6ab7462a6a5f618d175848a12c9c73cd57ffa602
Inna <shvartz_i@mail.ru>
2022-01-11 17:16:22 +0300

    Task2 added

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int[] array = new int[12];
+int sum = 0;
+
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(0, 10);
+
+for (int i = 0; i < array.Length; i++)
+    Console.WriteLine(array[i]);
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+        sum = sum + array[i];
+}
+Console.WriteLine(sum);
+
